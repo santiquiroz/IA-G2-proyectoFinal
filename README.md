f41d96d772043659f82ae2afe6732c8c619a3f37
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-04-03 07:54:19 -0500

    fina python commit

--- README.md
+++ README.md
@@ -2,6 +2,62 @@
 Miembros del equipo:
 	Santiago Quiroz Upegui (squirozu@unal.edu.co)
 	Mirai Alejandro Kaneko ()
-	el negro....
-	
+	el negro...
+Notas:
+1. El intervalo de agrupamiento de los datos esta dado por defecto en 5 segundos.
+2. El intervalo de obtencion de datos esta dado por defecto en 4 dias para los datos de entrenamiento y 4 horas para los datos de evaluacion.
+3. Los datos de entrenamiento son tomados desde 2019-03-20 12:00:00 hasta 2019-03-24 12:00:00.
+4. Los datos de evaluacion son tomados desde 4 horas antes de la ejecucion del la obtencion de estos.
+Instrucciones de Instalacion y operacion:
+1. Descomprimir el proyecto
+2. Instalas las siguientes librerias con pip:
+    - elasticsearch
+    - pandas
+    - json
+    - xlswriter
+    - openpyxl
+    - numpy
+3. Ejecutar main.py
+4. Con la opción 3 obtener los datos de entrenamiento y evaluación
+5. Con la opción 4 abrir las opciones de etiquetado:
+    - Con la opción 3 etiquetar todos los datos
+6. Salir con opción 5
+
+7. Abrir Weka
+8. Abrir package manager de Weka:
+9. Buscar e instalar los siguientes paquetes:
+    - wekaExcel
+    - wekaODF
+10. Cerrar package manager
+11. Abrir explorer en Weka
+12. Dar click en el botón "Open File"
+13. En la carpeta de entrenamiento seleccionar "trainingSINDATOS-ETIQUETADOS.xlsx"
+14. Dar click en "classify"     
+15. Dar clock en "choose"
+16. Seleccionar en "trees", "J48"
+17. En "test options" seleccionar "use training set"
+18. Seleccionar clase "ETIQUETADO"
+19. Click en "Start"
+20. Click derecho sobre el arbol y guardar el modelo (save model)
+21. Cerrar el explorer
+22. Abrir el explorer
+23. Click en "Open File"
+24. En la carpeta de evaluacion seleccionar "evauluationSINDATOS-ETIQUETADOS.xlsx"
+25. Click en "Save"
+26. Guardar como arff
+27. Click en la pestaña "classify"
+28. En "test options" seleccionar "supplied set"
+29. Click en el botón "set"
+30. Abrir el archivo arff
+31. Cambiar clase a ETIQUETADO y cerrar
+32. Click en "more options"
+33. En "output predictions" dar click en "choose"
+34. Seleccionar Plain Text
+35. Click en "ok"
+36. Bajo test options cambiar la clase a ETIQUETADO
+37. Click derecho en el recuadro vacío
+38. Dar click en "Load model"
+39. Seleccionar el modelo guardado
+40. Click derecho en el modelo cargado
+41. Dar click en "Re-evaluate model on current test set"	
 
--- python/main.py
+++ python/main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import json as json
 import csv
 import xlsxwriter
+import openpyxl
+import numpy as np
 
 
 from datetime import datetime, date, time, timedelta
@@ -13,9 +15,6 @@ import calendar
 """
 #obtiene los datos en un intervalo de tiempo y los guarda en un arreglo 
 def getInterval(es,inferior, superior):
-    
-
-    print("Obteniendo datos con scan")
     
     cres = helpers.scan(es,index="test-index", query={"query": {
     "bool": {
@@ -78,7 +77,7 @@ def getTotal(es,inferior,fechaFinal,intervaloSegundos,directorio):
     return True
 def getTrainingData(es,intervaloSegundos = 5):
     directorio = "./Entrenamiento/training"
-    fechaInicial= datetime.strptime('2019-03-24 11:00:00', '%Y-%m-%d %H:%M:%S')
+    fechaInicial= datetime.strptime('2019-03-20 12:00:00', '%Y-%m-%d %H:%M:%S')
     fechaFinal= datetime.strptime('2019-03-24 12:00:00', '%Y-%m-%d %H:%M:%S')
     print("inicial: "+fechaInicial.strftime('%Y-%m-%d %H:%M:%S'))
     print("final: "+fechaFinal.strftime('%Y-%m-%d %H:%M:%S'))
@@ -88,22 +87,90 @@ def getTrainingData(es,intervaloSegundos = 5):
 def getActualData(es,intervaloSegundos = 5):
     directorio = "./Evaluacion/evaluation"
     fechaFinal= datetime.now()
-    fechaInicial= fechaFinal-timedelta(hours=1)
+    fechaInicial= fechaFinal-timedelta(hours=4)
     print("inicial: "+fechaInicial.strftime('%Y-%m-%d %H:%M:%S'))
     print("final: "+fechaFinal.strftime('%Y-%m-%d %H:%M:%S'))
     getTotal(es,fechaInicial,fechaFinal,intervaloSegundos,directorio)
     
     return True
+def clasificar(url,numeroAnteriores=30):
+  doc = openpyxl.load_workbook(url)
+  hoja=doc.get_sheet_by_name("hoja1")
+  #.cell(row=1,column=1).value
+  cambioAnteriores=[]
+  anterior = hoja.cell(row=2,column=4).value
+  hoja.cell(row=2,column=3).value = "normal"  
+  siguiente = hoja.cell(row=3,column=4).value
+  hoja.cell(row=3,column=3).value = "normal"
+  cambio=0
+  if siguiente != None:
+    cambio=abs(int(anterior)-int(siguiente))/int(anterior)
+    cambioAnteriores.append(cambio)
+  else:
+    print("Error: campo nulo.")
+    return  True
+  print(anterior)
+  print(cambio)
+  print(cambioAnteriores)
+  print(np.average(cambioAnteriores))
+  i=4
+  anterior = siguiente
+  siguiente =siguiente= hoja.cell(row=i,column=4).value
+  while(siguiente != None):
+    
+    cambio=abs(int(anterior)-int(siguiente))/int(anterior)
+    if(cambio>(np.average(cambioAnteriores)+0.2)):
+      hoja.cell(row=i,column=3).value = "anormal"
+      cambio=cambioAnteriores[-1]
+      if(len(cambioAnteriores)==numeroAnteriores):
+        cambioAnteriores=cambioAnteriores[1:]
+        cambioAnteriores.append(cambio)
+      else:
+        cambioAnteriores.append(cambio) 
+    else:
+      hoja.cell(row=i,column=3).value = "normal" 
+      if(len(cambioAnteriores)==numeroAnteriores):
+        cambioAnteriores=cambioAnteriores[1:]
+        cambioAnteriores.append(cambio)
+      else:
+        cambioAnteriores.append(cambio)
 
+    anterior = siguiente
+    i = i+1
+    siguiente= hoja.cell(row=i,column=4).value
+  nombre=url.split('.')
+  doc.save("."+nombre[1]+"-ETIQUETADOS.xlsx")
+  return True
+def etiquetar():
+    cual=int(input("Que datos desea etiquetar? \n 1.entrenamiento. \n 2.evaluacion. \n 3.todos los anteriores.\n"))
+    if (cual == 1):
+      clasificar("./Entrenamiento/trainingSINDATOS.xlsx")
+    elif (cual == 2):
+      clasificar("./Entrenamiento/trainingSINDATOS.xlsx")
+    elif (cual == 3):
+      clasificar("./Entrenamiento/trainingSINDATOS.xlsx")
+      clasificar("./Evaluacion/evaluationSINDATOS.xlsx")
+    else:
+      print("Ha ingresado una opcion no valida")
+    return True
 if __name__ == "__main__":
     es = Elasticsearch(['54.147.121.162'],
     #http_auth=('user', 'secret'), 
     scheme="http",
     port=9200,
     timeout=30)
-    opcion= int(input("Ingrese 1 para obtener datos de entrenamiento o otro nuemero para evaluacion."))
-    if opcion == 1:
-        getTrainingData(es)
-    else:
-        getActualData(es)
-         
+    while(True):
+        opcion= int(input("Elija una opcion: \n 1.Obtener datos de entrenamiento. \n 2.obtener datos de evaluacion. \n 3.obtener datos de entrenamiento y evaluacion. \n 4.Etiquetar.\n 5.Salir.\n"))
+        if opcion == 1:
+            getTrainingData(es)
+        elif ( opcion == 2):
+            getActualData(es)
+        elif ( opcion ==3):
+            getTrainingData(es)
+            getActualData(es)
+        elif ( opcion == 4):
+            etiquetar()
+        elif ( opcion == 5):
+            break
+        else:
+          print("Ha ingresado una opcion no valida")        
